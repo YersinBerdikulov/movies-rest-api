a9e454c3619bd1a62d2f106394f51b74fc955ebe
Yersin Berdikulov <221225@astanait.edu.kz>
2025-03-16 18:18:17 +0500

    Add movie repository and abstractions to inject into service container

---
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -0,0 +1,50 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    private readonly List<Movie> _movies = [];
+
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        _movies.Add(movie);
+        
+        return Task.FromResult(true);
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        var movie = _movies.SingleOrDefault(m => m.Id == id);
+        
+        return Task.FromResult(movie);
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        var items = _movies.AsEnumerable();
+        
+        return Task.FromResult(items);
+    }
+
+    public Task<bool> UpdateAsync(Movie movie)
+    {
+        var movieIndex = _movies.IndexOf(movie);
+        if (movieIndex == -1)
+        {
+            return Task.FromResult(false);
+        }
+        
+        _movies[movieIndex] = movie;
+        
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        var removedCount = _movies.RemoveAll(m => m.Id == id);
+        var isRemoved = removedCount > 0;
+        
+        return Task.FromResult(isRemoved);
+    }
+}
